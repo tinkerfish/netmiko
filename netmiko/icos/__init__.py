41951730b6549242b9f95367b6548889811d63b5
reagans <reagans@u2264e7ca97085c.ant.amazon.com>
2020-03-12 15:08:11 -0700

    Initial ICOS support commit

---
+++ netmiko/icos/__init__.py
@@ -0,0 +1,4 @@
+from netmiko.icos.icos_ssh import IcosSSH
+
+
+__all__ = ["IcosSSH"]
---
+++ netmiko/icos/icos_ssh.py
@@ -0,0 +1,5 @@
+from netmiko.cisco_base_connection import CiscoSSHConnection
+
+
+class IcosSSH(CiscoSSHConnection):
+    pass
