d9efb6f6bb3ae0c6d334b4ee5c5e2fefa1d009c9
Tinkerfish <slreagan90@gmail.com>
2020-03-18 15:33:28 -0700

    added modified class functions

--- netmiko/icos/icos_ssh.py
+++ netmiko/icos/icos_ssh.py
@@ -2,4 +2,23 @@ from netmiko.cisco_base_connection import CiscoSSHConnection
 
 
 class IcosSSH(CiscoSSHConnection):
-    pass
+    """
+    Implement methods for interacting with Broadcom ICOS based devices.
+
+    """
+
+    def disable_paging(self, *args, **kwargs):
+        """ICOS does not have paging by default"""
+        return ""
+
+    def config_mode(self, config_command="configure"):
+        """Enter configuration mode."""
+        return super().config_mode(config_command=config_command)
+
+    def save_config(
+        self, cmd="write memory", confirm=True, confirm_response="y",
+    ):
+        """Saves Config"""
+        return super().save_config(
+            cmd=cmd, confirm=confirm, confirm_response=confirm_response
+        )
